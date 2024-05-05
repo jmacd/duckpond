c70fcb109aa7e775b0e9df3e454685dc9de5ad04
Joshua MacDonald <jmacd@lightstep.com>
2024-05-05 00:36:24 -0700

    wip arrow record batch

--- src/hydrovu.rs
+++ src/hydrovu.rs
@@ -26,6 +26,7 @@ use parquet::{
 };
 
 use std::collections::BTreeMap;
+use std::collections::BTreeSet;
 use std::env;
 use std::sync::Arc;
 
@@ -193,6 +194,9 @@ struct instrument {
     fields: Vec<FieldRef>,
 }
 
+use arrow::array::Int64Builder;
+use arrow::array::Float64Builder;
+
 pub fn read() -> Result<(), Error> {
     let client = Rc::new(Client::new(creds()?)?);
     let now = Utc::now();
@@ -239,24 +243,79 @@ pub fn read() -> Result<(), Error> {
 		.reduce(|s, t| format!("{}.{}", s, t))
 		.context("location has no parameters")?;
 
-	    let fname = format!("data-{}.parquet", schema);
-	    let mut inst = instrument{
-		schema: schema,
-		file: File::create(fname).with_context(|| format!("open parquet file {}", fname))?,
-		fields: vec![
-		    Arc::new(Field::new("location_id", DataType::Int64, false)),
-		    Arc::new(Field::new("timestamp", DataType::Int64, false)),
-		],
-	    };
+	    let mut inst = insts.get(schema);
+
+	    match inst {
+		Some(_) => (),
+		None => {
+		    let fname = format!("data-{}-{}.parquet", loc.location_id, schema);
+		    inst = instrument{
+			schema: schema,
+			file: File::create(fname).with_context(|| format!("open parquet file {}", fname))?,
+			fields: vec![
+			    Arc::new(Field::new("timestamp", DataType::Int64, false)),
+			],
+		    };
+		}
+	    }
+	    let inst = inst.unwrap();
 	    
 	    for p in one.parameters {
 		let pu = vu.lookup_param_unit(p);
 		inst.fields.push(
-		    Arc::new(Field::new(format!("{}.{}", pu.0, pu.1), DataType::Float64, false)),
+		    Arc::new(Field::new(format!("{}.{}", pu.0, pu.1), DataType::Float64, true)),
 		);
 	    }
+
+	    // one timestamp builder, N float64 builders
+	    let mut tsb = Int64Builder::default();
+	    let mut vb: Vec<_> = one.parameters.iter().map(|| Float64Builder::default()).collect();
+
+	    // compute unique timestamps
+	    let allTs = one.parameters
+		.iter()
+		.map(|p| p.readings.iter().map(|r| r.timestamp).collect())
+		.reduce(|x, y| x.into_iter().chain(y).collect())
+		.collect::<BTreeSet<i64>>();
+
+	    // compute an empty vector per timestamp
+	    let allVecs: BTreeMap<i64, Vec<Option<f64>>> = allTs.map(|ts| (ts, Vec::new(one.parameters.len)));
+
+	    // fill the vectors, leaving None if not set
+	    for i in 0..one.parameters.len {
+		for &r in one.parameters[i] {
+		    allVecs.get_mut(r.timestamp).unwrap().insert(i, Some(r.value));
+		}
+	    }
+
+	    for (ts, values) in allVecs {
+		tsb.append_value(ts);
+
+		for i in 0..values.len {
+		    vb[i].append_option(values[i]);
+		}
+	    }
+
+	    let mut sa: StructArray::new();
+
+        // let i32 = Arc::new(self.i32.finish()) as ArrayRef;
+        // let i32_field = Arc::new(Field::new("i32", DataType::Int32, false));
+
+        // let string = Arc::new(self.string.finish()) as ArrayRef;
+        // let string_field = Arc::new(Field::new("i32", DataType::Utf8, false));
+
+        // let i32_list = Arc::new(self.i32_list.finish()) as ArrayRef;
+        // let value_field = Arc::new(Field::new("item", DataType::Int32, true));
+        // let i32_list_field = Arc::new(Field::new("i32_list", DataType::List(value_field), true));
+
+        // StructArray::from(vec![
+        //     (i32_field, i32),
+        //     (string_field, string),
+        //     (i32_list_field, i32_list),
+        // ])
 	    
-            records.push(one);
+	    RecordBatch::from(&sa)
+
             std::thread::sleep(time::Duration::from_millis(100));
         }
     }
@@ -281,3 +340,59 @@ pub fn read() -> Result<(), Error> {
 
     Ok(())
 }
+
+// /// A custom row representation
+// struct MyRow {
+//     i32: i32,
+//     optional_i32: Option<i32>,
+//     string: Option<String>,
+//     i32_list: Option<Vec<Option<i32>>>,
+// }
+
+// /// Converts `Vec<Row>` into `StructArray`
+// #[derive(Debug, Default)]
+// struct MyRowBuilder {
+//     i32: Int32Builder,
+//     string: StringBuilder,
+//     i32_list: ListBuilder<Int32Builder>,
+// }
+
+// impl MyRowBuilder {
+//     fn append(&mut self, row: &MyRow) {
+//         self.i32.append_value(row.i32);
+//         self.string.append_option(row.string.as_ref());
+//         self.i32_list.append_option(row.i32_list.as_ref().map(|x| x.iter().copied()));
+//     }
+
+//     /// Note: returns StructArray to allow nesting within another array if desired
+//     fn finish(&mut self) -> StructArray {
+//         let i32 = Arc::new(self.i32.finish()) as ArrayRef;
+//         let i32_field = Arc::new(Field::new("i32", DataType::Int32, false));
+
+//         let string = Arc::new(self.string.finish()) as ArrayRef;
+//         let string_field = Arc::new(Field::new("i32", DataType::Utf8, false));
+
+//         let i32_list = Arc::new(self.i32_list.finish()) as ArrayRef;
+//         let value_field = Arc::new(Field::new("item", DataType::Int32, true));
+//         let i32_list_field = Arc::new(Field::new("i32_list", DataType::List(value_field), true));
+
+//         StructArray::from(vec![
+//             (i32_field, i32),
+//             (string_field, string),
+//             (i32_list_field, i32_list),
+//         ])
+//     }
+// }
+
+// impl<'a> Extend<&'a MyRow> for MyRowBuilder {
+//     fn extend<T: IntoIterator<Item = &'a MyRow>>(&mut self, iter: T) {
+//         iter.into_iter().for_each(|row| self.append(row));
+//     }
+// }
+
+// /// Converts a slice of [`MyRow`] to a [`RecordBatch`]
+// fn rows_to_batch(rows: &[MyRow]) -> RecordBatch {
+//     let mut builder = MyRowBuilder::default();
+//     builder.extend(rows);
+//     RecordBatch::from(&builder.finish())
+// }
