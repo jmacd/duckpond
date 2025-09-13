b3f89698066dd5f4af271f60e6f44293e5d8ecd7
Joshua MacDonald <josh.macdonald@gmail.com>
2025-09-13 09:37:25 -0700

    more script

--- test-hydrovu-dynamic-dir.sh
+++ test-hydrovu-dynamic-dir.sh
@@ -14,23 +14,25 @@ cargo run --bin pond mknod dynamic-dir /test-locations $CONFIG_FILE
 
 echo "✅ Dynamic directory created!"
 
-#echo "📋 Listing virtual FileSeries in /test-locations..."
 cargo run --bin pond list '/test-locations/**'
 
-#cargo run --bin pond cat '/test-locations/BDock'
-#cargo run --bin pond cat '/test-locations/Silver' --query "select count(*) from series"
-#cargo run --bin pond cat '/test-locations/Silver' --query "select count(*) from series"
-#cargo run --bin pond cat '/test-locations/Princess' 
+cargo run --bin pond list '/hydrovu/devices/**'
 
-#cargo run --bin pond query --show
+echo "✅ First detect-overlaps"
 
 cargo run --bin pond detect-overlaps "/hydrovu/devices/**/SilverVulink*.series"
+
+echo "✅ First set temporal override"
 
 cargo run --bin pond set-temporal-bounds /hydrovu/devices/6582334615060480/SilverVulink1.series \
   --min-time "2024-01-01 00:00:00" \
   --max-time "2024-05-30 23:59:59"
 
+echo "✅ Next detect-overlap should see the effect"
+
 cargo run --bin pond detect-overlaps "/hydrovu/devices/**/SilverVulink*.series"
+
+echo "✅ Should not print out-of-range points"
 
 cargo run --bin pond cat /hydrovu/devices/6582334615060480/SilverVulink1.series
 
