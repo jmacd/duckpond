270f00b3082acd0c57159315983cda8684eed8d7
Joshua MacDonald <josh.macdonald@gmail.com>
2025-10-06 20:28:59 -0700

    file

---
+++ noyo/run.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+ROOT=/Volumes/sourcecode/src/duckpond
+NOYO=${ROOT}/noyo
+POND=${NOYO}/pond
+EXE=${ROOT}/target/release/pond
+
+export RUST_LOG=info
+export POND
+
+cargo build --release
+
+${EXE} hydrovu run ${NOYO}/hydrovu.yaml
