3753f0438463b45d7ba3d520505529e8a31d864a
Joshua MacDonald <josh.macdonald@gmail.com>
2025-11-20 22:26:16 -0800

    export pass

--- noyo/export.sh
+++ noyo/export.sh
@@ -21,7 +21,7 @@ cargo build --release
 rm -rf ${OUTDIR}
 
 # Parameters
-#${EXE} export --pattern '/reduced/single_param/*/*.series' --pattern '/templates/params/param=*' --dir ${OUTDIR} --temporal "year,month"
+${EXE} export --pattern '/reduced/single_param/*/*.series' --pattern '/templates/params/param=*' --dir ${OUTDIR} --temporal "year,month"
 
 # Site detail
 ${EXE} export --pattern '/reduced/single_site/*/*.series' --pattern '/templates/sites/site=*' --dir ${OUTDIR} --temporal "year,month"
