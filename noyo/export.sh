d09b5c4dcce736ce3c465dc945c50080fe47c14e
Joshua MacDonald <josh.macdonald@gmail.com>
2025-12-25 20:38:35 -0800

    testing real hydro

--- noyo/export.sh
+++ noyo/export.sh
@@ -5,8 +5,8 @@ set -x -i
 ROOT=/Volumes/sourcecode/src/duckpond
 NOYO=${ROOT}/noyo
 POND=${NOYO}/pond
-#EXE=${ROOT}/target/release/pond
-EXE=${ROOT}/target/debug/pond
+EXE=${ROOT}/target/release/pond
+#EXE=${ROOT}/target/debug/pond
 OUTDIR=./export
 
 export RUST_BACKTRACE=1
@@ -21,7 +21,7 @@ cargo build
 rm -rf ${OUTDIR}
 
 # Parameters
-${EXE} export --pattern '/reduced/single_param/*/*.series' --pattern '/templates/params/param=*' --dir ${OUTDIR} --temporal "year,month"
+samply record ${EXE} export --pattern '/reduced/single_param/*/*.series' --pattern '/templates/params/param=*' --dir ${OUTDIR} --temporal "year,month"
 
 # Site detail
-${EXE} export --pattern '/reduced/single_site/*/*.series' --pattern '/templates/sites/site=*' --dir ${OUTDIR} --temporal "year,month"
+#${EXE} export --pattern '/reduced/single_site/*/*.series' --pattern '/templates/sites/site=*' --dir ${OUTDIR} --temporal "year,month"
